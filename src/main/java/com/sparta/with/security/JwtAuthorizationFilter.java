ef9c11baafb93bf96dca7396aa51336bd6ea1db6
SongIsak <thddltkr10@naver.com>
2023-08-08 00:13:30 +0900

    add-refresh-token

--- src/main/java/com/sparta/with/security/JwtAuthorizationFilter.java
+++ src/main/java/com/sparta/with/security/JwtAuthorizationFilter.java
@@ -1,12 +1,14 @@
 package com.sparta.with.security;
 
+import com.sparta.with.entity.RefreshToken;
 import com.sparta.with.jwt.JwtUtil;
+import com.sparta.with.repository.RefreshTokenRepository;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -16,29 +18,52 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+import java.util.UUID;
+
 @Slf4j(topic = "JWT 검증 및 인가")
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
     private final UserDetailsServiceImpl userDetailsService;
+    private final RefreshTokenRepository refreshTokenRepository;
 
-    public JwtAuthorizationFilter(JwtUtil jwtUtil, UserDetailsServiceImpl userDetailsService) {
+
+    public JwtAuthorizationFilter(JwtUtil jwtUtil, UserDetailsServiceImpl userDetailsService, RefreshTokenRepository refreshTokenRepository) {
         this.jwtUtil = jwtUtil;
         this.userDetailsService = userDetailsService;
+        this.refreshTokenRepository = refreshTokenRepository;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain) throws ServletException, IOException {
-
         String tokenValue = jwtUtil.getJwtFromHeader(req);
+        String refreshToken = req.getHeader("RefreshToken");
 
         if (StringUtils.hasText(tokenValue)) {
+            boolean isValid = false;
+            try {
+                isValid = jwtUtil.validateToken(tokenValue, refreshToken);
+            } catch (ExpiredJwtException e){
+                RefreshToken refToken = refreshTokenRepository.findById(refreshToken)
+                        .orElseThrow(() -> new IllegalArgumentException("리프레시 실패"));
 
-            if (!jwtUtil.validateToken(tokenValue)) {
+                isValid = true;
+                Long id = refToken.getMemberId();
+                UserDetails userDetails = userDetailsService.loadUserById(id);
+
+                String refreshTokenVal = UUID.randomUUID().toString();
+                refreshTokenRepository.delete(refToken);
+                refreshTokenRepository.save(new RefreshToken(refreshTokenVal, id));
+                tokenValue = jwtUtil.createToken(userDetails.getUsername(), ((UserDetailsImpl) userDetails).getRole())
+                        .substring(7);
+                res.addHeader("RefreshToken", refreshTokenVal);
+                res.addHeader(JwtUtil.AUTHORIZATION_HEADER, tokenValue);
+            }
+            if (!isValid) {
                 log.error("Token Error");
                 return;
             }
-
             Claims info = jwtUtil.getUserInfoFromToken(tokenValue);
 
             try {
